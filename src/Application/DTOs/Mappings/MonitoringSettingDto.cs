9600befbf98b3e164d2418d034cb11c1e58f69a2
maxim <maxim_shelest78@mail.ru>
2025-03-04 23:02:02 +0300

    [HW-2] испарвил тип данных CheckInterval в MonitoringSettingDto

--- src/Application/DTOs/Mappings/MonitoringSettingDto.cs
+++ src/Application/DTOs/Mappings/MonitoringSettingDto.cs
@@ -5,6 +5,6 @@ public class MonitoringSettingDto
     public int Id { get; set; }
     public int ServiceId { get; set; }
     public ServiceDto ServiceDto { get; set; }
-    public DateTime CheckInterval { get; set; }
+    public string CheckInterval { get; set; }
     public bool Mode { get; set; }
 }
