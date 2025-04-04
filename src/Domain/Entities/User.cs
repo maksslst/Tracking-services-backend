4b32a9898586515ef2822ed38b952dfb35b7f437
maxim <maxim_shelest78@mail.ru>
2025-04-04 22:46:10 +0300

    [HW-5]

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -5,19 +5,11 @@ namespace Domain.Entities;
 public class User
 {
     public int Id { get; set; }
-    
-    [Required]
-    public string Username { get; set; }
-    
-    [Required]
-    public string FirstName { get; set; }
-    
-    [Required]
-    public string LastName { get; set; }
+    public required string Username { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
     public string? Patronymic { get; set; }
-    
-    [Required]
-    public string Email { get; set; }
+    public required string Email { get; set; }
     public int CompanyId { get; set; }
     public Company? Company { get; set; }
 }
